53976c9e721baa7e5ecbca71b3780585702a4554
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-10 17:55:49 -0300

    criando uma url para o app autenticacao

--- imobi/imobi/urls.py
+++ imobi/imobi/urls.py
@@ -1,8 +1,9 @@
 # arquivo que defini as rotas da aplicaçaõ
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('autenticacao.urls'))
 ]
