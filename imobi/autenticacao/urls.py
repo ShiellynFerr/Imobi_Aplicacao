bfd94a10b17eef115a7c2051e1d5d4a60891be56
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-11 15:28:10 -0300

    corrigindo indentação

--- imobi/autenticacao/urls.py
+++ imobi/autenticacao/urls.py
@@ -3,5 +3,5 @@ from .import views
 urlpatterns = [
   # função que recebe  a requisição do usuario e retorna uma response
   path('cadastro/', views.cadastro, name = "cadastro" ),
-   path('logar/', views.logar , name = "logar" ),
+  path('logar/', views.logar , name = "logar" ),
 ]
