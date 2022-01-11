85e4a7c09f0a4d17feb3db219a082e9a68e3cad7
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-11 12:33:07 -0300

    corrigindo erro de sintaxe

--- imobi/autenticacao/urls.py
+++ imobi/autenticacao/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
-urlpatterns = {
+urlpatterns = [
   # função que recebe  a requisição do usuario e retorna uma response
   path('cadastro/', views.cadastro, name = "cadastro" ),
-   path('login/', views.login, name = "login" )
-}
+   path('logar/', views.logar , name = "logar" ),
+]
