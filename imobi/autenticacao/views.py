962be1acdd05d4f98d3077b1d4117a8e2588f0fe
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-11 15:25:55 -0300

    adicionando as mensagens na views

--- imobi/autenticacao/views.py
+++ imobi/autenticacao/views.py
@@ -1,38 +1,57 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User # importando tabela usuario
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib import auth
 
 
  #irá retornar uma response = aquilo que é exibido para o usuário
-def cadastro (request):
-  if request.method == "GET" :
-   return render(request, 'cadastro.html')
-  elif request.method == "Post":
-   username = request.Post.get("username")
-  email = request.Post.get("email")
-  senha = request.Post.get("senha")
+def cadastro(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
 
-  if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
-    return redirect('/auth/cadastro') 
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/auth/cadastro')
 
-   #variavel chamada user// trazendo os dados atráves do username
+        user = User.objects.filter(username=username)
 
-    user = User.objects.filter(username==username)
-
-    if user.exists() :
-      return redirect('/auth/cadastro') 
-    
-    try:
-      user = User(username = username, email = email, password = senha) #instancia de usuario
-
-      #salvando no banco de dados
-      user.save()
-      return redirect('/auth/logar') #redirecionando para a pagina de login
-    except:
-      return redirect('/auth/cadastro') #caso de algum erro, redireciona para a pagina de cadastro
-
-  return(HttpResponse)
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse nome cadastrado')
+            return redirect('/auth/cadastro')
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
+            return redirect('/auth/logar')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/auth/cadastro')
+ 
 
 def logar (request):
-  return HttpResponse('logar')
+  if request.method == "GET":
+    if request.user.is_authenticated:
+        return redirect('/')
+    return render(request, 'login.html')
+  elif request.method == "POST":
+    username = request.POST.get(username)
+    senha= request.POST.get(senha)
 
+    usuario = auth.authenticate(username=username, password=senha)
+
+    if not usuario:
+      messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
+      return redirect('/auth/logar')
+    else:
+      auth.login(request,usuario)
+      return redirect('/')
