6ad6ce6350b69f855475b68757eb0aa4c7d7fa8f
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-13 19:07:14 -0300

    cadastrando novas tabelas no admin

--- imobi/plataforma/admin.py
+++ imobi/plataforma/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import  Imagem, Cidade, DiasVisita, Horario, Imovei,Visitas
 
-# Register your models here.
+@admin.register(Imovei)
+class ImoveiAdmin(admin.ModelAdmin):
+ list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
+ list_editable = ('valor', 'tipo')
+ list_filter = ('cidade', 'tipo')
+
+admin.site.register(Cidade)
+admin.site.register(Horario)
+admin.site.register(Visitas)
+admin.site.register(Imagem)
+admin.site.register(DiasVisita)
