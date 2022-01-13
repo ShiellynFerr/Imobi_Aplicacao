e1f475d76a5b6a55e8ee74f6b69081228b8f62ad
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-13 12:51:41 -0300

    referenciando o novo app no core da aplicação

--- imobi/imobi/settings.py
+++ imobi/imobi/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autenticacao'
+    'autenticacao',
+    'plataforma'
 ]
 
 MIDDLEWARE = [
