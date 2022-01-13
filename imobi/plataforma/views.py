c55f4f26905beeea6162112016004cdb707a7e25
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-13 14:36:10 -0300

    teste plataforma

--- imobi/plataforma/views.py
+++ imobi/plataforma/views.py
@@ -1,5 +1,8 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url="/auth/logar") #a pagina só pode ser acessada se o usuario estive logado
 def home(request):
-  return HttpResponse('Estou na Home')
+  return HttpResponse('home')
