cbca50c25f855ed8cd42352f19424d281bbff600
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-13 15:43:18 -0300

    criando o banco de dados

--- imobi/plataforma/models.py
+++ imobi/plataforma/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Imagem(models.Model):
+    img = models.ImageField(upload_to='img')
+
+    def str(self) -> str:
+        return self.img.url
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=30)
+
+    def str(self) -> str:
+        return self.nome
+
+class DiasVisita(models.Model):
+    dia = models.CharField(max_length=20)
+
+    def str(self) -> str:
+        return self.dia
+
+class Horario(models.Model):
+    horario = models.TimeField()
+
+    def str(self) -> str:
+        return str(self.horario)
+
+class Imovei(models.Model):
+    choices = (('V', 'Venda'),
+               ('A', 'Aluguel'))
+
+    choices_imovel = (('A', 'Apartamento'),
+                      ('C', 'Casa'))
+
+    imagens = models.ManyToManyField(Imagem)
+    valor = models.FloatField()
+    quartos = models.IntegerField()
+    tamanho = models.FloatField()
+    cidade = models.ForeignKey(Cidade, on_delete=models.DO_NOTHING)
+    rua = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=1, choices=choices)
+    tipo_imovel = models.CharField(max_length=1, choices=choices_imovel)
+    numero = models.IntegerField()
+    descricao = models.TextField()
+    dias_visita = models.ManyToManyField(DiasVisita)
+    horarios = models.ManyToManyField(Horario)
+
+    def str(self) -> str:
+        return self.rua
+
+class Visitas(models.Model):
+    choices = (('S', 'Segunda'),
+                ('T', 'Terça'),
+                ('Q', 'Quarta'),
+                ('QI', 'Quinta'),
+                ('SE', 'Sexta'),
+                ('SA', 'Sabado'),
+                ('D', 'Domingo'))
+
+    choices_status = (('A', 'Agendado'),
+                      ('F', 'Finalizado'),
+                      ('C', 'Cancelado'))
+    imovel = models.ForeignKey(Imovei, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    dia = models.CharField(max_length=20)
+    horario = models.TimeField()
+    status = models.CharField(max_length=1, choices=choices_status, default="A")
+
+
+    def str(self) -> str:
+        return self.usuario.username       
+
