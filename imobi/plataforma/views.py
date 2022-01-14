665e88ad6924572b95d2791fabaf0cb5e4fab2de
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-13 22:32:21 -0300

    lógica para filtrar os imoveis

--- imobi/plataforma/views.py
+++ imobi/plataforma/views.py
@@ -2,9 +2,28 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Imovei
+from .models import Cidade, Imovei
 
 @login_required(login_url="/auth/logar") #a pagina só pode ser acessada se o usuario estive logado
 def home(request):
-  imoveis = Imovei.objects.all
-  return render(request, 'home.html', {'imoveis': imoveis})
+  preco_minimo = request.GET.get('preco_minimo')
+  preco_maximo = request.GET.get('preco_maximo')
+  cidade = request.GET.get('cidade')
+  tipo = request.GET.getlist('tipo')
+  if preco_minimo or preco_maximo or cidade or tipo:
+
+#Verificação do filtro
+    if not preco_minimo:
+       preco_minimo = 0
+    if not preco_maximo:
+         preco_maximo = 999999999999
+    if not tipo:
+         tipo = ['A', 'C']
+
+   
+    imoveis = Imovei.objects.filter(valor__gte=preco_minimo).filter(valor__lte=preco_maximo).filter(tipo_imovel__in=tipo).filter(cidade=cidade)
+  else:
+    imoveis = Imovei.objects.all()
+
+  cidades = Cidade.objects.all
+  return render(request, 'home.html', {'imoveis': imoveis, 'cidades': cidades})
