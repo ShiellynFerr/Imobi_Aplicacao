684938839243dbb7ac3db8406138be30606152dc
Shiellyn Ferreira <brunashiellyn@gmail.com>
2022-01-13 23:35:45 -0300

    criando url para visualizar um imovel especifico

--- imobi/plataforma/urls.py
+++ imobi/plataforma/urls.py
@@ -1,6 +1,9 @@
+from http.client import HTTPResponse
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('imovel/<str:id>', views.imovel, name="imovel"),
 ]
+
--- imobi/plataforma/views.py
+++ imobi/plataforma/views.py
@@ -1,8 +1,8 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Cidade, Imovei
+from .models import Imovei, Cidade
+from django.shortcuts import get_object_or_404
 
 @login_required(login_url="/auth/logar") #a pagina só pode ser acessada se o usuario estive logado
 def home(request):
@@ -27,3 +27,8 @@ def home(request):
 
   cidades = Cidade.objects.all
   return render(request, 'home.html', {'imoveis': imoveis, 'cidades': cidades})
+
+def imovel(request, id):
+    imovel = get_object_or_404(Imovei, id=id)
+    print(imovel)
+    return render(request, 'imovel.html', {'imovel': imovel, 'sugestoes': sugestoes, 'id': id})
